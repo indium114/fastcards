6055b21402544e92413cc0048efe7d7b689021b4
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-15 14:04:52 +0200

    feat: create deck command

---
+++ cmd/create.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/stikypiston/fastcards/internal"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new deck",
+	Run: func(cmd *cobra.Command, args []string) {
+		deck := internal.Deck{
+			ID:    internal.NewID(),
+			Name:  args[0],
+			Cards: []internal.Card{},
+		}
+
+		if err := internal.SaveDeck(deck); err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+
+		fmt.Println("Deck created: ", deck.Name)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
