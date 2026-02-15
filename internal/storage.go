eb0b740efe986e33955f056f1119263874d6f143
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-15 13:56:37 +0200

    feat: Storage functions

---
+++ internal/storage.go
@@ -0,0 +1,48 @@
+package internal
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+func baseDir() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".fastcards")
+}
+
+func decksDir() string {
+	return filepath.Join(baseDir(), "decks")
+}
+
+func EnsureDirs() error {
+	return os.MkdirAll(decksDir(), 0755)
+}
+
+func deckPath(name string) string {
+	return filepath.Join(decksDir(), name+".json")
+}
+
+func SaveDeck(deck Deck) error {
+	if err := EnsureDirs(); err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(deck, "", " ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(deckPath(deck.Name), data, 0644)
+}
+
+func LoadDeck(name string) (Deck, error) {
+	var deck Deck
+	data, err := os.ReadFile(deckPath(name))
+	if err != nil {
+		return deck, err
+	}
+
+	err = json.Unmarshal(data, &deck)
+	return deck, err
+}
