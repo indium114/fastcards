e4a2c1cbb2652dab74d1e8eb39dac5252a738735
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-15 14:46:28 +0200

    feat: Make the study view look prettier

--- ui/study.go
+++ ui/study.go
@@ -5,19 +5,55 @@ import (
 
 	"github.com/stikypiston/fastcards/internal"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+type keyMap struct {
+	Flip key.Binding
+	Yes  key.Binding
+	No   key.Binding
+	Quit key.Binding
+}
+
+func newKeyMap() keyMap {
+	return keyMap{
+		Flip: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "flip"),
+		),
+		Yes: key.NewBinding(
+			key.WithKeys("y"),
+			key.WithHelp("y", "correct"),
+		),
+		No: key.NewBinding(
+			key.WithKeys("n"),
+			key.WithHelp("n", "incorrect"),
+		),
+		Quit: key.NewBinding(
+			key.WithKeys("q", "ctrl+c"),
+			key.WithHelp("q", "quit"),
+		),
+	}
+}
 
 type Model struct {
 	due      []internal.DueRef
 	index    int
 	showBack bool
 	done     bool
+
+	keys keyMap
+	help help.Model
 }
 
 func NewStudyModelFromRefs(refs []internal.DueRef) *Model {
 	return &Model{
-		due: refs,
+		due:  refs,
+		keys: newKeyMap(),
+		help: help.New(),
 	}
 }
 
@@ -34,22 +70,23 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case tea.KeyMsg:
-		switch msg.String() {
 
-		case "ctrl+c", "q":
+		switch {
+
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
 
-		case "enter":
+		case key.Matches(msg, m.keys.Flip):
 			m.showBack = true
 
-		case "y":
+		case key.Matches(msg, m.keys.Yes):
 			if m.showBack {
 				ref := m.due[m.index]
 				internal.Promote(&ref.Deck.Cards[ref.Idx])
 				m.next()
 			}
 
-		case "n":
+		case key.Matches(msg, m.keys.No):
 			if m.showBack {
 				ref := m.due[m.index]
 				internal.Reset(&ref.Deck.Cards[ref.Idx])
@@ -73,15 +110,44 @@ func (m *Model) next() {
 func (m *Model) View() string {
 
 	if m.done {
-		return "\nAll done.\n"
+		return "\nAll done.\nPress [q] to quit."
 	}
 
 	ref := m.due[m.index]
 	card := ref.Deck.Cards[ref.Idx]
 
-	if !m.showBack {
-		return fmt.Sprintf("\n\n%s\n", card.Front)
+	content := card.Front
+	if m.showBack {
+		content = fmt.Sprintf("%s\n\n%s", card.Front, card.Back)
 	}
 
-	return fmt.Sprintf("\n\n%s\n\n%s\n", card.Front, card.Back)
+	// Styled card box
+	box := lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		Padding(1, 2).
+		Render(content)
+
+	progress := fmt.Sprintf("Card %d/%d\n\n", m.index+1, len(m.due))
+
+	return progress + box + "\n\n" + m.help.View(m)
+}
+
+func (m *Model) ShortHelp() []key.Binding {
+	return []key.Binding{
+		m.keys.Flip,
+		m.keys.Yes,
+		m.keys.No,
+		m.keys.Quit,
+	}
+}
+
+func (m *Model) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{
+			m.keys.Flip,
+			m.keys.Yes,
+			m.keys.No,
+			m.keys.Quit,
+		},
+	}
 }
