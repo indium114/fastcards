89e5d491983c91a3f83e72deb23354f4e5eb550b
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-15 14:09:04 +0200

    feat: add card command

---
+++ cmd/add.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/stikypiston/fastcards/internal"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a card to a deck",
+	Run: func(cmd *cobra.Command, args []string) {
+		deck, err := internal.LoadDeck(args[0])
+		if err != nil {
+			fmt.Println("Deck not found")
+			return
+		}
+
+		card := internal.Card{
+			ID:    internal.NewID(),
+			Front: args[1],
+			Back:  args[2],
+			State: 1,
+		}
+
+		deck.Cards = append(deck.Cards, card)
+
+		if err := internal.SaveDeck(deck); err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+
+		fmt.Println("Card added")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
