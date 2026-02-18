9182127100ec275e763da8a8365a27da424c02e5
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-18 17:52:39 +0200

    feat: archive remove command

---
+++ cmd/archiveremove.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/stikypiston/fastcards/internal"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var archiveremoveCmd = &cobra.Command{
+	Use:   "add <file>",
+	Short: "Add a deck to the Deck Archive",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		deck := args[0] + ".json"
+
+		newpath := filepath.Join(internal.DecksDir(), deck)
+		oldpath := filepath.Join(internal.ArchiveDir(), deck)
+
+		err := os.Rename(oldpath, newpath)
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		fmt.Println("Archived deck:", deck)
+	},
+}
+
+func init() {
+	archiveCmd.AddCommand(archiveCmd)
+}
